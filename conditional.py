e1cbe3a24ca602ffa15bcee260b743485b40eed8
Kurtis Garcia <kurtisgarcia14@gmail.com>
2020-12-09 16:52:04 -0500

    These are my Day 3 commits

---
+++ conditional.py
@@ -0,0 +1,15 @@
+MAGIC_NUMBER = 6
+user_input = int(input("Please type a number: "))
+
+if user_input == MAGIC_NUMBER:
+    print("Whoa!!! Are you a mind reader?")
+else:
+    print("Sorry, try again next time!")
+
+
+#Hey, hi python!!
+#Does what the user typed (user_input) match the magic number (MAGIC_NUMBER)?
+#If it does, will you please tell the user: "Whoa!!! Are you a mind reader?"
+#If it doesn't, will you please tell the user: "Sorry, try again next time!"
+#And then you can stop running the program
+#Thanks Python!!!!
---
+++ madlib.py
@@ -0,0 +1,4 @@
+name = input("what is name?")
+subject = input("what is subject?")
+
+print(name + "'s " + "favorite subject in school is " + subject.lower())
